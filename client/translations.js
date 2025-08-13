7634d6fe9d91518fcf40ab5d44d63f5b86dcaca7
Martin Babača <martin.babaca1@gmail.com>
2025-08-13 14:17:13 +0200

    [fea] Localization
    * delete feature
    
    * [fea] remove done flag on click
    
    * [add] localization

---
+++ client/translations.js
@@ -0,0 +1,37 @@
+const translations = {
+  en: {
+    title: "Task Manager",
+    placeholder: "New task",
+    add: "Add",
+    removeDone: "🔒 Remove Done Tasks",
+    removeTooltip: "Removes all completed tasks (requires password)"
+  },
+  de: {
+    title: "Aufgabenmanager",
+    placeholder: "Neue Aufgabe",
+    add: "Hinzufügen",
+    removeDone: "🔒 Erledigte Aufgaben löschen",
+    removeTooltip: "Löscht alle erledigten Aufgaben (Passwort erforderlich)"
+  },
+  sk: {
+    title: "Správca úloh",
+    placeholder: "Nová úloha",
+    add: "Pridať",
+    removeDone: "🔒 Odstrániť dokončené úlohy",
+    removeTooltip: "Odstráni všetky dokončené úlohy (vyžaduje heslo)"
+  },
+  cz: {
+    title: "Správce úkolů",
+    placeholder: "Nový úkol",
+    add: "Přidat",
+    removeDone: "🔒 Odstranit hotové úkoly",
+    removeTooltip: "Odstraní všechny hotové úkoly (vyžaduje heslo)"
+  },
+  pl: {
+    title: "Menedżer zadań",
+    placeholder: "Nowe zadanie",
+    add: "Dodaj",
+    removeDone: "🔒 Usuń ukończone zadania",
+    removeTooltip: "Usuwa wszystkie ukończone zadania (wymaga hasła)"
+  }
+};
